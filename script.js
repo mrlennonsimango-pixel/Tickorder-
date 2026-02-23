b849fa18098e41669230c6b32e3aef2ff06271a8
mrlennonsimango-pixel <mrlennonsimango@gmail.com>
2026-02-23 16:21:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const products = [
-  { name: "Wireless Earbuds", price: 49 },
-  { name: "Smart Watch", price: 89 },
-  { name: "Portable Speaker", price: 39 }
+  { id: 1, name: "Wireless Earbuds", price: 49 },
+  { id: 2, name: "Smart Watch", price: 89 },
+  { id: 3, name: "Portable Speaker", price: 39 }
 ];
+
+let cart = [];
 
 const productContainer = document.getElementById("products");
 
@@ -10,11 +12,24 @@ products.forEach(product => {
   const card = document.createElement("div");
   card.classList.add("product-card");
 
+  const button = document.createElement("button");
+  button.textContent = "Add to Cart";
+
+  button.addEventListener("click", () => {
+    addToCart(product.id);
+  });
+
   card.innerHTML = `
     <h3>${product.name}</h3>
     <p>$${product.price}</p>
-    <button>Buy Now</button>
   `;
 
+  card.appendChild(button);
   productContainer.appendChild(card);
 });
+
+function addToCart(id) {
+  const product = products.find(p => p.id === id);
+  cart.push(product);
+  alert(product.name + " added to cart!");
+};
