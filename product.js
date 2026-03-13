826242a458a2940a4f287fd53252c682961e1627
mrlennonsimango-pixel <mrlennonsimango@gmail.com>
2026-03-13 09:36:02 +0200

    Create product.js

---
+++ product.js
@@ -0,0 +1,23 @@
+const products = [
+  {
+    id: "p001",
+    name: "Men's T-Shirt",
+    price: 199,
+    stock: 10,
+    image: "assets/images/product1.jpg"
+  },
+  {
+    id: "p002",
+    name: "PU Leather Handbag",
+    price: 499,
+    stock: 5,
+    image: "assets/images/product2.jpg"
+  },
+  {
+    id: "p003",
+    name: "Wireless Earbuds",
+    price: 899,
+    stock: 0,
+    image: "assets/images/product3.jpg"
+  }
+];
