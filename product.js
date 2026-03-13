6a7a37f3493cbd7317460a37009b3bc84ef2605c
mrlennonsimango-pixel <mrlennonsimango@gmail.com>
2026-03-13 18:17:33 +0200

    Update product.js

--- product.js
+++ product.js
@@ -1,10 +1,40 @@
 // --- Products ---
 const products = [
-  { id: 1, name: "Brown Handbag", price: 120, image: "assets/Images/brownhandbag.jpg" },
-  { id: 2, name: "Red Handbag", price: 120, image: "assets/Images/redhandbag.jpg" },
-  { id: 3, name: "Pestal Green Handbag", price: 120, image: "assets/Images/greenhandbag.jpg" },
-  { id: 4, name: "Black Handbag", price: 120, image: "assets/Images/blackhandbag.jpg" },
-  { id: 5, name: "White Handbag", price: 120, image: "assets/Images/whitehandbag.jpg" }
+  {
+    id: 1,
+    name: "Brown Handbag",
+    price: 120,
+    weight: 1, // 1 kg
+    image: "assets/Images/brownhandbag.jpg"
+  },
+  {
+    id: 2,
+    name: "Red Handbag",
+    price: 150,
+    weight: 1,
+    image: "assets/Images/redhandbag.jpg"
+  },
+  {
+    id: 3,
+    name: "Pestal Green Handbag",
+    price: 130,
+    weight: 1,
+    image: "assets/Images/greenhandbag.jpg"
+  },
+  {
+    id: 4,
+    name: "Black Handbag",
+    price: 140,
+    weight: 1,
+    image: "assets/Images/blackhandbag.jpg"
+  },
+  {
+    id: 5,
+    name: "White Handbag",
+    price: 135,
+    weight: 1,
+    image: "assets/Images/whitehandbag.jpg"
+  }
 ];
 
 // --- Cart ---
