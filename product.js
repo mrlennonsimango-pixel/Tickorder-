13e402f72f27d8cf236a508f10ece3ed4abded4b
mrlennonsimango-pixel <mrlennonsimango@gmail.com>
2026-03-13 16:35:27 +0200

    Update product.js

--- product.js
+++ product.js
@@ -1,10 +1,10 @@
 // --- Products ---
 const products = [
   { id: 1, name: "Brown Handbag", price: 120, image: "assets/Images/brownhandbag.jpg" },
-  { id: 2, name: "Red Handbag", price: 150, image: "assets/Images/redhandbag.jpg" },
-  { id: 3, name: "Pestal Green Handbag", price: 130, image: "assets/Images/greenhandbag.jpg" },
-  { id: 4, name: "Black Handbag", price: 140, image: "assets/Images/blackhandbag.jpg" },
-  { id: 5, name: "White Handbag", price: 135, image: "assets/Images/whitehandbag.jpg" }
+  { id: 2, name: "Red Handbag", price: 120, image: "assets/Images/redhandbag.jpg" },
+  { id: 3, name: "Pestal Green Handbag", price: 120, image: "assets/Images/greenhandbag.jpg" },
+  { id: 4, name: "Black Handbag", price: 120, image: "assets/Images/blackhandbag.jpg" },
+  { id: 5, name: "White Handbag", price: 120, image: "assets/Images/whitehandbag.jpg" }
 ];
 
 // --- Cart ---
