04dc213cb320a3e01e29cf6b84acd73eb25c4c8f
mrlennonsimango-pixel <mrlennonsimango@gmail.com>
2026-03-24 16:25:03 +0200

    Update product.js

--- product.js
+++ product.js
@@ -1,12 +1,11 @@
+// product.js
+const products = [
+  { id: 1, name: "Brown Handbag", price: 120, weight: 1, image: "assets/Images/brownhandbag.jpg" },
+  { id: 2, name: "Red Handbag", price: 150, weight: 1, image: "assets/Images/redhandbag.jpg" },
+  { id: 3, name: "Pestal Green Handbag", price: 130, weight: 1, image: "assets/Images/greenhandbag.jpg" },
+  { id: 4, name: "Black Handbag", price: 140, weight: 1, image: "assets/Images/blackhandbag.jpg" },
+  { id: 5, name: "White Handbag", price: 135, weight: 1, image: "assets/Images/whitehandbag.jpg" }
+];
 
-
-  // --- Products ---
-  const products = [
-    { id: 1, name: "Brown Handbag", price: 120, weight: 1, image: "assets/Images/brownhandbag.jpg" },
-    { id: 2, name: "Red Handbag", price: 150, weight: 1, image: "assets/Images/redhandbag.jpg" },
-    { id: 3, name: "Pestal Green Handbag", price: 130, weight: 1, image: "assets/Images/greenhandbag.jpg" },
-    { id: 4, name: "Black Handbag", price: 140, weight: 1, image: "assets/Images/blackhandbag.jpg" },
-    { id: 5, name: "White Handbag", price: 135, weight: 1, image: "assets/Images/whitehandbag.jpg" }
-  ];
 
   
