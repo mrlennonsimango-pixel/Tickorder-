ad8acfeab2727de044228517ec7437b03c0ec161
mrlennonsimango-pixel <mrlennonsimango@gmail.com>
2026-03-26 17:56:47 +0200

    Update product.js

--- product.js
+++ product.js
@@ -1,24 +1,15 @@
 const products = [
   { 
-    id: 1, 
-    name: "Handbag", 
-    weight: 1,
+    id: 1,
+    name: "Handbag",
+    category: "Handbags",
     colors: [
-      { name: "Brown", image: "assets/Images/brownhandbag1.jpg", price: 120 },
-      { name: "Red", image: "assets/Images/redhandbag1.jpg", price: 150 },
-      { name: "Black", image: "assets/Images/blackhandbag1.jpg", price: 140 }
+      { name: "Brown", image: "assets/Images/brownhandbag.jpg", price: 120 },
+      { name: "Red", image: "assets/Images/redhandbag.jpg", price: 150 },
+      { name: "Pestal Green", image: "assets/Images/greenhandbag.jpg", price: 130 },
+      { name: "Black", image: "assets/Images/blackhandbag.jpg", price: 140 },
+      { name: "White", image: "assets/Images/whitehandbag.jpg", price: 135 }
     ],
-    description: "Stylish handbag, available in multiple colors."
-  },
-  { 
-    id: 2, 
-    name: "Backpack", 
-    weight: 1.2,
-    colors: [
-      { name: "Green", image: "assets/Images/greenbackpack1.jpg", price: 130 },
-      { name: "Blue", image: "assets/Images/bluebackpack1.jpg", price: 140 }
-    ],
-    description: "Durable backpack for everyday use."
+    weight: 1
   }
 ];
-  
