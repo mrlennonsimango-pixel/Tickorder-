18d6c63f2803f7a73e12efc26f5deed247ceea01
mrlennonsimango-pixel <mrlennonsimango@gmail.com>
2026-03-28 10:16:37 +0200

    Update product.js

--- product.js
+++ product.js
@@ -11,24 +11,24 @@ const products = [
       { name: "White", image: "assets/Images/whitehandbag.jpg", price: 135 }
     ],
     weight: 1
-  }
+  },
 
-  {
-  id: 2,
-  name: "Gamazine 20L",
-  category: "Paints",
-  image: "assets/Images/gamazine-20l.jpg",
-  price: 399,
-  size: "20 Litres",
-  finish: "Textured, decorative",
-  coverage: "±6–8 m² per 20L",
-  application: "Steel trowel",
-  features: [
-    "Durable",
-    "Waterproof",
-    "Low maintenance",
-    "Crack-resistant"
-  ],
-  description: "Perfect for feature walls, ceilings, facades, and residential or commercial projects."
-}
+  { 
+    id: 2,
+    name: "Gamazine 20L",
+    category: "Paints",
+    image: "assets/Images/gamazine-20l.jpg",
+    price: 399,
+    size: "20 Litres",
+    finish: "Textured, decorative",
+    coverage: "±6–8 m² per 20L",
+    application: "Steel trowel",
+    features: [
+      "Durable",
+      "Waterproof",
+      "Low maintenance",
+      "Crack-resistant"
+    ],
+    description: "Perfect for feature walls, ceilings, facades, and residential or commercial projects."
+  }
 ];
