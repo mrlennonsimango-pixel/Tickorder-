c80b5eb0c5c1852ab1b0a3ad28a1211b3a52fe59
mrlennonsimango-pixel <mrlennonsimango@gmail.com>
2026-03-28 12:57:20 +0200

    Update product.js

--- product.js
+++ product.js
@@ -1,22 +1,44 @@
 const products = [
-  {
+  { 
     id: 1,
-    name: "Handbag",
+    parentId: 1,
+    name: "Handbag - Brown",
     category: "Handbags",
-    colors: [
-      { name: "Brown", image: "assets/Images/brownhandbag.jpg", price: 120, link: "product.html?id=1&color=brown" },
-      { name: "Red", image: "assets/Images/redhandbag.jpg", price: 150, link: "product.html?id=1&color=red" },
-      { name: "Green", image: "assets/Images/greenhandbag.jpg", price: 130, link: "product.html?id=1&color=green" }
-    ],
+    image: "assets/Images/brownhandbag.jpg",
+    price: 120,
+    color: "Brown",
+    link: "product.html?id=1&color=brown",
     description: "Stylish and durable handbag."
   },
-  {
+  { 
     id: 2,
+    parentId: 1,
+    name: "Handbag - Red",
+    category: "Handbags",
+    image: "assets/Images/redhandbag.jpg",
+    price: 150,
+    color: "Red",
+    link: "product.html?id=1&color=red",
+    description: "Stylish and durable handbag."
+  },
+  { 
+    id: 3,
+    parentId: 1,
+    name: "Handbag - Green",
+    category: "Handbags",
+    image: "assets/Images/greenhandbag.jpg",
+    price: 130,
+    color: "Green",
+    link: "product.html?id=1&color=green",
+    description: "Stylish and durable handbag."
+  },
+  { 
+    id: 4,
     name: "Gamazine 20L",
     category: "Paints",
     image: "assets/Images/gamazine-20l.jpg",
     price: 399,
-    link: "product.html?id=2",
+    link: "product.html?id=4",
     description: "Perfect for feature walls, ceilings, facades, and residential or commercial projects."
   }
 ];
