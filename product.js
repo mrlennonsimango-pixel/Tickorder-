5f309501ca103998b1762d01536eea7773c82b3e
mrlennonsimango-pixel <mrlennonsimango@gmail.com>
2026-03-28 16:30:41 +0200

    Update product.js

--- product.js
+++ product.js
@@ -7,51 +7,46 @@ const products = [
     image: "assets/Images/brownhandbag.jpg",
     price: 120,
     color: "Brown",
-    link: "product.html?id=1&color=brown",
     description: "Stylish and durable handbag."
   },
   { 
     id: 2,
     parentId: 1,
-    name: "handbag - Red",
-    category: "Handbags",
+    name: "Handbag - Red",
+    category: "handbags",
     image: "assets/Images/redhandbag.jpg",
     price: 150,
     color: "Red",
-    link: "product.html?id=1&color=red",
     description: "Stylish and durable handbag."
   },
   { 
     id: 3,
     parentId: 1,
-    name: "handbag - Green",
-    category: "Handbags",
+    name: "Handbag - Green",
+    category: "handbags",
     image: "assets/Images/greenhandbag.jpg",
     price: 130,
     color: "Green",
-    link: "product.html?id=1&color=green",
     description: "Stylish and durable handbag."
   },
   { 
     id: 4,
     parentId: 1,
-    name: "handbag - Black",
-    category: "Handbags",
+    name: "Handbag - Black",
+    category: "handbags",
     image: "assets/Images/blackhandbag.jpg",
     price: 140,
     color: "Black",
-    link: "product.html?id=1&color=black",
     description: "Stylish and durable handbag."
   },
   { 
     id: 5,
     parentId: 1,
-    name: "handbag - White",
-    category: "Handbags",
+    name: "Handbag - White",
+    category: "handbags",
     image: "assets/Images/whitehandbag.jpg",
     price: 135,
     color: "White",
-    link: "product.html?id=1&color=white",
     description: "Stylish and durable handbag."
   },
   { 
@@ -60,7 +55,6 @@ const products = [
     category: "paints",
     image: "assets/Images/gamazine-20l.jpg",
     price: 399,
-    link: "product.html?id=6",
     description: "Perfect for feature walls, ceilings, facades, and residential or commercial projects."
   }
 ];
